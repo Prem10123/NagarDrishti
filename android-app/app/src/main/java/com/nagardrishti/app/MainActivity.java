2274dadb255e8a9d032c143ff99d8cadd3ebda89
Prem10123 <premsuthar1012@gmail.com>
2026-09-05 01:48:51 +0530

    Newer UI

--- android-app/app/src/main/java/com/nagardrishti/app/MainActivity.java
+++ android-app/app/src/main/java/com/nagardrishti/app/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         requestRuntimePermissions();
 
         webView = findViewById(R.id.webView);
-        webView.setBackgroundColor(0xFFF4EFE6);
+        webView.setBackgroundColor(0xFF070B12);
         webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
         CookieManager.getInstance().setAcceptCookie(true);
         CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
